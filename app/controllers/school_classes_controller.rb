4b029d9d778bcdf62788c9641069bcc34635a877
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-12-04 21:47:16 +0000

    Automatically backed up by Learn

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,19 @@
+class SchoolClassesController < ApplicationController
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
